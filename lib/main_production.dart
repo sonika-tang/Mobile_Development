a0705805030857d5a1febe2445a5bcefed94769a
sonika-tang <sonikatang630@gmail.com>
2026-02-26 22:59:10 +0700

    Implement main_production.dart

---
+++ lib/main_production.dart
@@ -0,0 +1,16 @@
+
+import 'package:mobile/data/repositories/songs/song_repository_remote.dart';
+import 'package:provider/provider.dart';
+
+import 'data/repositories/songs/song_repository.dart';
+import 'main_common.dart';
+
+List<Provider> get providersProduction {
+  return [
+    Provider<SongRepository>(create: (context) => SongRepositoryRemote()),
+  ];
+}
+
+void main() {
+  mainCommon(providersProduction);
+}
