324aa9aeef75cac02100a63437a0b6b47745d5e7
sonika-tang <sonikatang630@gmail.com>
2026-02-26 22:50:07 +0700

    start code

---
+++ lib/main_dev.dart
@@ -0,0 +1,15 @@
+import 'package:mobile/data/repositories/songs/song_repository_mock.dart';
+import 'package:provider/provider.dart';
+
+
+import 'data/repositories/songs/song_repository.dart';
+import 'main_common.dart';
+
+/// Configure provider dependencies for dev environement
+List<Provider> get providersLocal {
+  return [Provider<SongRepository>(create: (context) => SongRepositoryMock())];
+}
+
+void main() {
+  mainCommon(providersLocal);
+}
