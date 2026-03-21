a532fef827e33634de496f309a930c0460f30b80
Sonika Tang <sonikatang630@gmail.com>
2026-03-21 22:02:43 +0700

    Delete lib/Week9 directory
    Push the wrong directory

--- lib/Week9/data/repositories/settings/app_settings_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../../../model/settings/app_settings.dart';
-
-abstract class AppSettingsRepository {
- 
-  Future<AppSettings> load();
-
-
-  Future<void> save(AppSettings settings);
-}
--- lib/Week9/data/repositories/songs/song_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../../../model/songs/song.dart';
-
-abstract class SongRepository {
-  Future<List<Song>> fetchSongs();
-  
-  Future<Song?> fetchSongById(String id);
-}
